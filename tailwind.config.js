3a2570bc7afbc0615000377c6e54c26dd1f10e3c
Thilagavsanr <vtk33347@gmail.com>
2025-11-02 13:05:48 +0530

    Initialize Tailwind CSS config and .gitignore
    Add Tailwind CSS configuration and .gitignore file.

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+
+### **9. .gitignore**
+```
+# Dependencies
+node_modules/
+/.pnp
+.pnp.js
+
+# Testing
+/coverage
+
+# Production
+/build
+
+# Misc
+.DS_Store
+.env.local
+.env.development.local
+.env.test.local
+.env.production.local
+
+npm-debug.log*
+yarn-debug.log*
+yarn-error.log*
